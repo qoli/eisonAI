e77024264c8bac336e5d12c5c5793cbb859b4e0d
Ronnie W. <llqoli@gmail.com>
2025-12-25 14:39:29 +0800

    💄 style(SwiftikToken): 統一程式碼縮排與格式
    - 將 `#else` 區塊內的程式碼統一縮排，使其與 `#if SWIFT_PACKAGE` 區塊的格式一致
    - 移除 `public struct Tiktoken` 定義前後的額外空行，使結構更緊湊
    - 統一內部屬性與方法宣告之間的空行間距

--- SwiftikToken/SwiftikToken.swift
+++ SwiftikToken/SwiftikToken.swift
@@ -4,20 +4,20 @@ private extension Bundle {
     // Provides a bundle similar to SwiftPM's Bundle.module, but works in app/framework targets too.
     static var swiftikModule: Bundle {
         #if SWIFT_PACKAGE
-        return .module
+            return .module
         #else
-        // Try to find the bundle where the resources are located.
-        // 1) If this code is in a framework, use the bundle for this type.
-        let bundleForType = Bundle(for: _BundleMarker.self)
-        if bundleForType != Bundle.main {
-            return bundleForType
-        }
-        // 2) If running inside an app, try the main bundle first.
-        if let resourceBundleURL = Bundle.main.url(forResource: "SwiftikToken", withExtension: "bundle"),
-           let resourceBundle = Bundle(url: resourceBundleURL) {
-            return resourceBundle
-        }
-        return Bundle.main
+            // Try to find the bundle where the resources are located.
+            // 1) If this code is in a framework, use the bundle for this type.
+            let bundleForType = Bundle(for: _BundleMarker.self)
+            if bundleForType != Bundle.main {
+                return bundleForType
+            }
+            // 2) If running inside an app, try the main bundle first.
+            if let resourceBundleURL = Bundle.main.url(forResource: "SwiftikToken", withExtension: "bundle"),
+               let resourceBundle = Bundle(url: resourceBundleURL) {
+                return resourceBundle
+            }
+            return Bundle.main
         #endif
     }
 }
@@ -26,18 +26,17 @@ private extension Bundle {
 private class _BundleMarker {}
 
 public struct Tiktoken {
-    
     enum Error: Swift.Error {
         case vocabularyFileNotFound
     }
-    
+
     let encoding: Encoding
     private let loader = Loader()
-    
+
     public init(encoding: Encoding) {
         self.encoding = encoding
     }
-    
+
     public func encode(
         text: String,
         allowedSpecial: Set<String> = Set(),
@@ -49,7 +48,7 @@ public struct Tiktoken {
         ) else {
             throw Error.vocabularyFileNotFound
         }
-        
+
         let encoder = try await loader.load(fileURL: fileURL)
         let regex = try encoding.pattern.makeRegex()
         let tokenEncoder = TokenEncoder(
@@ -57,7 +56,7 @@ public struct Tiktoken {
             specialTokenEncoder: encoding.specialTokenEncoder,
             regex: regex
         )
-        
+
         return try Tokenizer(
             encoder: tokenEncoder,
             specialTokens: encoding.specialTokens
@@ -68,4 +67,3 @@ public struct Tiktoken {
         )
     }
 }
-
