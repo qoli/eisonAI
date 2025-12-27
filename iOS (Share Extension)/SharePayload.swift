626f33fc5ac48210dd6b9cf96f86b22ade3af45c
Ronnie W. <llqoli@gmail.com>
2025-12-27 19:49:05 +0800

    🗑️ chore(iOS): 移除 iOS 分享擴充功能
    - 刪除整個 iOS (Share Extension) 目錄
    - 移除 Info.plist 設定檔
    - 移除 SharePayload 資料模型
    - 移除 SharePayloadStore 儲存工具
    - 移除 ShareViewController 主控制器
    - 【清理】移除不再使用的分享擴充功能程式碼

--- iOS (Share Extension)/SharePayload.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct SharePayload: Codable, Identifiable, Equatable {
-    let id: String
-    let createdAt: Date
-    let url: String?
-    let text: String?
-    let title: String?
-}
